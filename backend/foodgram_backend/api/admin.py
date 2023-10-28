d62fcd5e2a4f509481b5a536d78134be40885404
Aleksander <therealchistyakov@ya.ru>
2023-10-28 14:49:00 +0300

    Добавлены настройки в админку

--- backend/foodgram_backend/api/admin.py
+++ backend/foodgram_backend/api/admin.py
@@ -1,28 +1,54 @@
 from django.contrib import admin
-from user.models import User, Follow
-from recipe.models import Tag, Ingredient, Recipe
 
-admin.site.register(User)
+from user.models import User, Follow
+from recipe.models import Tag, Ingredient, Recipe, Favorite
+
 admin.site.register(Follow)
 admin.site.register(Tag)
-admin.site.register(Ingredient)
-admin.site.register(Recipe)
 
 
-class UserFilter(admin.ModelAdmin):
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'first_name', 'last_name')
     list_filter = ('username', 'email')
 
 
 admin.site.unregister(User)
-admin.site.register(User, UserFilter)
+admin.site.register(User, UserAdmin)
 
 
-class RecipeFilter(admin.ModelAdmin):
-    list_display = ('name', 'author', 'tags',
-                    'ingredients', 'image',
-                    'cooking_time', 'pub_date')
+class FavoriteInstanceInline(admin.TabularInline):
+    model = Favorite
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author', 'display_tags', 'get_favorites_count',)
     list_filter = ('author', 'name', 'tags')
+    fields = ('name', 'author', 'tags',
+              'ingredients', 'image', 'text',
+              'cooking_time', )
+    inlines = [FavoriteInstanceInline]
+
+    def display_tags(self, obj):
+        return ", ".join([tag.name for tag in obj.tags.all()])
+
+    display_tags.short_description = 'Теги'
+
+    @admin.display(description='В избранном')
+    def get_favorites_count(self, obj):
+        return obj.favorites.count()
+
 
 admin.site.unregister(Recipe)
-admin.site.register(Recipe, RecipeFilter)
+admin.site.register(Recipe, RecipeAdmin)
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'measurement_unit')
+    list_filter = ('name',)
+
+
+admin.site.unregister(Ingredient)
+admin.site.register(Ingredient, IngredientAdmin)
--- backend/foodgram_backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/foodgram_backend/settings.py
@@ -1,18 +1,15 @@
+# flake8: noqa
 import os
 from datetime import timedelta
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-%x1o=6b17ql9a146&@p)9ko@6vrlglv(qsdkum2phvna-+t64k'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
@@ -21,7 +18,6 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -73,9 +69,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'foodgram_backend.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -83,9 +76,6 @@ DATABASES = {
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -103,9 +93,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/3.2/topics/i18n/
-
 LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
@@ -117,13 +104,8 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = '/static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
